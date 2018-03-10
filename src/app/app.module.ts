d194d1cc6e9c1dbf1668a5bfe431b0ca8f8b3fc7
Gabriel Ferreira <Gabrielferrer@outlook.com.br>
2018-03-10 19:43:29 -0300

    Add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { AppComponent } from './app.component';
 import {MainModule} from "./main/main.module";
 import {SharedModule} from "./shared.module";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import {SharedModule} from "./shared.module";
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     SharedModule,
     MainModule
   ],
