7b35738aadc61b878c2de410d82811ba1d61ac0d
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2018-04-07 21:00:22 -0300

    Adicionando legenda e sufixo

--- src/app/main/data-insertion/data-insertion.component.ts
+++ src/app/main/data-insertion/data-insertion.component.ts
@@ -9,8 +9,19 @@ export class DataInsertionComponent implements OnInit {
 
   info: any;
 
+  selectSufixo: any[];
+
+
   constructor() {
     this.info = {};
+
+    this.selectSufixo = [
+      {id: 1, nome: 'Sem sufixo'},
+      {id: 2, nome: 'Metro'},
+      {id: 3, nome: 'Kg'},
+      {id: 4, nome: 'Centimetros'}
+  ];
+
   }
 
   ngOnInit() {
