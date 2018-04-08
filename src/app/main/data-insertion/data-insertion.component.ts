07eb18b8eac12cb8553a3c441aeec62861cee7e3
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2018-04-08 13:22:55 -0300

    Adicionando e removendo chips

--- src/app/main/data-insertion/data-insertion.component.ts
+++ src/app/main/data-insertion/data-insertion.component.ts
@@ -13,18 +13,51 @@ export class DataInsertionComponent implements OnInit {
 
 
   constructor() {
-    this.info = {};
+    this.info = {
+      dados: []
+    };
 
     this.selectSufixo = [
-      {id: 1, nome: 'Sem sufixo'},
-      {id: 2, nome: 'Metro'},
-      {id: 3, nome: 'Kg'},
-      {id: 4, nome: 'Centimetros'}
-  ];
+      { id: 1, nome: 'Sem sufixo' },
+      { id: 2, nome: 'Metro' },
+      { id: 3, nome: 'Kg' },
+      { id: 4, nome: 'Centimetros' }
+    ];
 
   }
 
   ngOnInit() {
+
   }
+
+  insertData() {
+    if (this.info.currentDado && this.info.currentDado.length) {
+      let isExists = false;
+      this.info.dados.forEach((dado) => {
+        if (dado.name === this.info.currentDado) {
+          dado.qtd++;
+          isExists = true;
+        }
+      });
+      if (!isExists) {
+        console.log('!isExists');
+        const obj = {
+          name: this.info.currentDado,
+          qtd: 1
+        }
+        this.info.dados.push(obj);
+      }
+      this.info.currentDado = null;
+    }
+  }
+
+  removeData(index) {
+    if(this.info.dados[index].qtd > 1) {
+      this.info.dados[index].qtd--;
+      return;
+    }
+      this.info.dados.splice(index, 1);
+    }
+
 
 }
