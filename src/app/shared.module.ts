2a11a4de46286b56584b35035be2350ab9826337
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2018-04-12 14:40:26 -0300

    Retirando googleCharts

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -7,15 +7,11 @@ import {RouterModule} from '@angular/router';
 import { SMNUIModule } from 'ng-smn-ui';
 import {UiToolbarService} from 'ng-smn-ui';
 
-import { Ng2GoogleChartsModule } from 'ng2-google-charts';
-
-
 @NgModule({
   exports: [
     FormsModule,
     BrowserModule,
     SMNUIModule,
-    Ng2GoogleChartsModule,
     HttpModule,
     HttpClientModule,
     RouterModule
