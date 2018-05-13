c9bd48664557ce9f893bd10bf04f744d93a9ba5d
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2018-05-13 13:19:17 -0300

    Mudando nome do projeto

--- dist/statistics-web/main.js
+++ dist/statistics-web/main.js
@@ -12677,7 +12677,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/gabriel/Documentos/projects/teste/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! /home/gabriel/Documentos/projects/statistics-web/src/main.ts */"./src/main.ts");
 
 
 /***/ })
