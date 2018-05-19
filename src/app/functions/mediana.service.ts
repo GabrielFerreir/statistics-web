c94030c71940f1d46249be6cc871f688ca9a226a
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2018-05-18 21:09:02 -0300

    Corrigndo mediana

--- src/app/functions/mediana.service.ts
+++ src/app/functions/mediana.service.ts
@@ -46,6 +46,9 @@ export class MedianaService {
         const pre = ((parseFloat(somatorio) / 2) - parseFloat(freqAA)) / parseFloat(freq);
         res.push(limiteInferior + (pre * interval));
       });
+
+      return (res[0] + res[1]) / 2;
+
       return res[0] === res[1] ? res[0] : res;
     } else {
       const pos = (somatorio + 1) / 2;
