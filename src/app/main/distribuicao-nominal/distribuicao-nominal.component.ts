309b219c38306e71f8d285cc4808d45e1bad8014
Gabriel <=>
2018-06-10 13:32:13 -0700

    Iniciando distribuição nominal

---
+++ src/app/main/distribuicao-nominal/distribuicao-nominal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-distribuicao-nominal',
+  templateUrl: './distribuicao-nominal.component.html',
+  styleUrls: ['./distribuicao-nominal.component.scss']
+})
+export class DistribuicaoNominalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/main/distribuicao-nominal/distribuicao-nominal.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DistribuicaoNominalComponent } from './distribuicao-nominal.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DistribuicaoNominalComponent]
+})
+export class DistribuicaoNominalModule { }
