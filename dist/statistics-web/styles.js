ba6ea3ed00f5d6b90867c07acc31aea218c92e1c
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2018-08-15 09:58:07 -0300

    Build

--- dist/statistics-web/styles.js
+++ dist/statistics-web/styles.js
@@ -539,7 +539,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\GABRIEL\Documents\projects\statistics-web\src\styles.scss */"./src/styles.scss");
+module.exports = __webpack_require__(/*! C:\Users\Gabriel Ferreira\statistics-web\src\styles.scss */"./src/styles.scss");
 
 
 /***/ })
